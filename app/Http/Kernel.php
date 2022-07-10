7b075ceb5a88ec268b7d8f1906cc6a58ddbd81da
Ahmed Salim <ahmed.salim@adamtravel.net>
2022-07-10 17:00:49 +0200

    fix endpoints

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -43,7 +43,6 @@ class Kernel extends HttpKernel
             // \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
             'throttle:api',
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
-            TenantAuthentication::class,
         ],
     ];
 
@@ -65,5 +64,6 @@ class Kernel extends HttpKernel
         'signed' => \Illuminate\Routing\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
+        'tenant-auth' => TenantAuthentication::class,
     ];
 }
--- routes/api.php
+++ routes/api.php
@@ -13,11 +13,16 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+Route::group(['middleware' => 'tenant-auth'] , function(){
+    Route::post('login',[\App\Http\Controllers\Api\Auth\LoginController::class,'login']);
+    Route::post('logout',[\App\Http\Controllers\Api\Auth\LoginController::class,'logout']);
 
-Route::post('login',[\App\Http\Controllers\Api\Auth\LoginController::class,'login']);
-Route::post('logout',[\App\Http\Controllers\Api\Auth\LoginController::class,'logout']);
+    Route::post('register',[\App\Http\Controllers\Api\Auth\RegisterController::class,'register']);
 
-Route::post('register',[\App\Http\Controllers\Api\Auth\RegisterController::class,'register']);
+    Route::get('/products',[\App\Http\Controllers\Api\Website\ProductController::class,'index']);
+    Route::get('/products/{id}',[\App\Http\Controllers\Api\Website\ProductController::class,'show']);
+});
 
-Route::get('/products',[\App\Http\Controllers\Api\Website\ProductController::class,'index']);
-Route::get('/products/{id}',[\App\Http\Controllers\Api\Website\ProductController::class,'show']);
+Route::group(['prefix' => 'admin','middleware' => 'auth:api'],function(){
+
+});
