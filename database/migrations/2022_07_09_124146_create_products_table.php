dd1ffaf98c08e5471c217d4ec640c1e2313f0e6c
Ahmed Salim <ahmed.salim@adamtravel.net>
2022-07-10 16:53:06 +0200

    add tenant id column to products table

--- database/migrations/2022_07_09_124146_create_products_table.php
+++ database/migrations/2022_07_09_124146_create_products_table.php
@@ -19,6 +19,7 @@ return new class extends Migration
             $table->float('price');
             $table->text('description');
             $table->string('image');
+            $table->foreignId('tenant_id')->constrained();
             $table->timestamps();
         });
     }
